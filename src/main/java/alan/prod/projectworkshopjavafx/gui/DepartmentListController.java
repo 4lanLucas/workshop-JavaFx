7c3ba3e1217d592c8368aaad858d912a99746908
4lan Lucas <alanlucas0707@gmail.com>
2024-04-17 07:52:06 -0300

    update for departmentList

--- src/main/java/alan/prod/projectworkshopjavafx/gui/DepartmentListController.java
+++ src/main/java/alan/prod/projectworkshopjavafx/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import alan.prod.projectworkshopjavafx.gui.util.Alerts;
 import alan.prod.projectworkshopjavafx.gui.util.Utils;
 import alan.prod.projectworkshopjavafx.model.entities.Department;
 import alan.prod.projectworkshopjavafx.model.services.DepartmentService;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -13,10 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -38,9 +36,31 @@ public class DepartmentListController implements Initializable, DataChangeListen
     @FXML
     private TableColumn<Department,String> tableColumnName;
     @FXML
+    private TableColumn<Department, Department> tableColumnEdit;
+    @FXML
     private Button btNew;
     private ObservableList<Department> obsList;
 
+    @FXML
+    private void initEditButtons(){
+         tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+         tableColumnEdit.setCellFactory(param -> new TableCell<Department,Department>(){
+             private final Button button = new Button("edit");
+
+             @Override
+             protected void updateItem(Department obj, boolean empty){
+                 super.updateItem(obj, empty);
+                 if(obj == null){
+                     setGraphic(null);
+                     return;
+                 }
+                 setGraphic(button);
+                 button.setOnAction(event -> {
+                     createDialogForm(obj,"DepartmentForm.fxml",Utils.currentStage(event));
+                 });
+             }
+        });
+    }
     @FXML
     public void onbtNewAction(ActionEvent event){
         Stage parentStage = Utils.currentStage(event);
@@ -73,6 +93,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();
     }
 
     private void createDialogForm(Department obj,String absoluteName, Stage parentStage){
