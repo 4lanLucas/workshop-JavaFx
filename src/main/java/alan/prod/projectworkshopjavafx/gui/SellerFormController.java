8dc4288f0c98f078ed6a8d86f9e3889fe50a1c87
4lan Lucas <alanlucas0707@gmail.com>
2024-04-17 10:32:40 -0300

    added new seller saving on database logic

--- src/main/java/alan/prod/projectworkshopjavafx/gui/SellerFormController.java
+++ src/main/java/alan/prod/projectworkshopjavafx/gui/SellerFormController.java
@@ -19,6 +19,7 @@ import javafx.scene.control.*;
 import javafx.util.Callback;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
@@ -101,9 +102,30 @@ public class SellerFormController implements Initializable {
             exception.addError("name","field cant be empty");
         }
         obj.setName(txtName.getText());
+
+        if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")){
+            exception.addError("email","field cant be empty");
+        }
+        obj.setEmail(txtEmail.getText());
+
+        if(dpBirthDate.getValue() == null){
+            exception.addError("birthDate","field cant be empty");
+        } else {
+            Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+            obj.setBirthDate(Date.from(instant));
+        }
+
+        if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")){
+            exception.addError("baseSalary","field cant be empty");
+        }
+        obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText().trim()));
+
+        obj.setDepartment(comboBoxDepartment.getValue());
+
         if(!exception.getErrors().isEmpty()){
             throw exception;
         }
+
         return obj;
     }
 
@@ -156,10 +178,13 @@ public class SellerFormController implements Initializable {
 
     private void setErrorMessage(Map<String,String> errors){
         Set<String> fields = errors.keySet();
-        if(fields.contains("name")){
-            labelErrorName.setText(errors.get("name"));
-        }
+        labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+        labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+        labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+        labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+
     }
+
     private void initializeComboBoxDepartment() {
         Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
             @Override
